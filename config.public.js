42d325b364c371587b990df86888efe642f55cd3
aparraut <262027511+aparraut@users.noreply.github.com>
2026-03-09 16:54:18 -0300

    Fix remote config loading by shipping config.public.js

---
+++ config.public.js
@@ -0,0 +1,5 @@
+window.SQL_MIND_TRAINER_CONFIG = {
+  supabaseUrl: "https://yuiwlbrfihyqoycsrhrc.supabase.co",
+  supabaseAnonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1aXdsYnJmaWh5cW95Y3NyaHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMjkxODUsImV4cCI6MjA3ODcwNTE4NX0.3bb8_qc1VVTV8cZNP3irUOAUDsArWHV-QlObvPtI8cM",
+};
